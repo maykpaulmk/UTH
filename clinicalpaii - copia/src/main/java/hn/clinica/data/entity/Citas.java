530a5f011f14e430985315d75b93c770381c9fe0
maykpaulmk <gerencia@terracomp.hn>
2023-07-12 00:07:08 -0600

    update
    update

---
+++ clinicalpaii - copia/src/main/java/hn/clinica/data/entity/Citas.java
@@ -0,0 +1,54 @@
+package hn.clinica.data.entity;
+
+import jakarta.persistence.Entity;
+import java.time.LocalDateTime;
+
+@Entity
+public class Citas extends AbstractEntity {
+
+    private Integer idcita;
+    private LocalDateTime fecha;
+    private String paciente;
+    private String direccion;
+    private String telefono;
+    private String detalle;
+
+    public Integer getIdcita() {
+        return idcita;
+    }
+    public void setIdcita(Integer idcita) {
+        this.idcita = idcita;
+    }
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
+    public void setFecha(LocalDateTime fecha) {
+        this.fecha = fecha;
+    }
+    public String getPaciente() {
+        return paciente;
+    }
+    public void setPaciente(String paciente) {
+        this.paciente = paciente;
+    }
+    public String getDireccion() {
+        return direccion;
+    }
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+  
+	public String getDetalle() {
+		return detalle;
+	}
+	public void setDetalle(String detalle) {
+		this.detalle = detalle;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+}
